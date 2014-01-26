6900b40353f695a1393c61797db07fc54fc2b313
namesn5 <namesn5@computersn5>
2014-01-27 04:05:36 +0900

    Maven project serial number 1 created

---
+++ root/trunks/MetaIntegrationFlavorEGPhilippovAngarskiyTake1MavenProjectSN1/src/main/java/groupidmetaintegrationprojectgroupsn1/artifactidflavoregphilippovangarskiytake1artifactsn1/App.java
@@ -0,0 +1,13 @@
+package groupidmetaintegrationprojectgroupsn1.artifactidflavoregphilippovangarskiytake1artifactsn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
